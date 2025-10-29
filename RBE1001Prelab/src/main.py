77342f9b72bddeb27cbf444d741759d47f807b57
LilyT18 <lilytangus@gmail.com>
2025-10-29 11:21:08 -0400

    Add motors to prelab

--- RBE1001Prelab/src/main.py
+++ RBE1001Prelab/src/main.py
@@ -15,5 +15,6 @@ brain=Brain()
 
 brain.screen.print("Hello V5")
 
-
+left_motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+right_motor = Motor(Ports.PORT2, GearSetting.RATIO_18_1, True)
         
