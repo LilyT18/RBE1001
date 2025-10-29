c36cf3dd613bf31f2c6d796adf2be66d3ed6b67f
LilyT18 <lilytangus@gmail.com>
2025-10-29 11:47:10 -0400

    Prelab stuff

--- RBE1001Prelab/src/main.py
+++ RBE1001Prelab/src/main.py
@@ -15,8 +15,8 @@ brain=Brain()
 
 brain.screen.print("Hello V5")
 
-left_motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
-right_motor = Motor(Ports.PORT2, GearSetting.RATIO_18_1, True)
+left_motor = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
+right_motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, True)
 
 left_motor.spin_for(FORWARD, 1, TURNS, 30, RPM, wait = False)
 right_motor.spin_for(FORWARD, 1, TURNS, 30, RPM, wait = False)
