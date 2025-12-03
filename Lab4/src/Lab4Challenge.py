5a3e37614d6e45538b57153bf9b5f3901c986bc6
LilyT18 <lilytangus@gmail.com>
2025-12-03 10:10:17 -0500

    Create Lab4Challenge.py

---
+++ Lab4/src/Lab4Challenge.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       lilyt                                                        #
+# 	Created:      12/3/2025, 10:08:41 AM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
