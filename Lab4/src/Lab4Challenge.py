d5fd22fb5909dd23b6086463bf76afbd988fd5e6
LilyT18 <lilytangus@gmail.com>
2025-12-03 10:27:19 -0500

    More code

--- Lab4/src/Lab4Challenge.py
+++ Lab4/src/Lab4Challenge.py
@@ -13,11 +13,14 @@ from vex import *
 # Brain should be defined by default
 brain = Brain()
 
-rightMotor = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
-leftMotor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, True)
+rightMotor = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False) #TODO: find motor #
+leftMotor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, True) #TODO: find motor #
 controller = Controller()
 rack1 = Motor(Ports.PORT3, GearSetting.RATIO_36_1, False) #Motor 58
 rack2 = Motor(Ports.PORT4, GearSetting.RATIO_36_1, True) #Motor 59
+ai_vision_15__Red_Folder = Colordesc(1, 222, 31, 63, 11, 0.48) #TODO: figure out what this means
+ai_vision_15 = AiVision(Ports.PORT15, ai_vision_15__Red_Folder) #TODO: confirm port
+bumper_g = Bumper(brain.three_wire_port.g) #TODO: confirm port
 
 speedOfTravel = 80 #RPM
 speedOfRack = 50 #RPM
@@ -39,15 +42,20 @@ def rackMove(direction, speed, distance):
 #States
 IDLE = 0
 GOTOSTAGE = 1 #Goes to specific height
-DRIVETOFRUIT = 2
+IDENTIFYCORRECTBIN = 2 #Uses camera to detect color of bin and match it with the fruit
 PICKFRUIT = 3 #Drives towards fruit and picks it
+GOTOISLE = 4 #Goes to spcific aisle (1, 2, or 3)
+GOTOBINS = 5 #Based off of current aisle, goes to the bin area
+DEPOSITFRUIT= 6 #Deposits fruit into correct bin
+RETURNTOBASE = 7 #Returns to starting position from bin area
 
 currentState = IDLE
 rackLevel = 0
 levelOne = .75 #Turns
 levelTwo = 2 #Turns
-driveDistance = 2.5 #Turns
+toFruitDistance = 2.5 #Turns
 
+#Used to detect motion completion
 wasMoving = False
 def checkMotionComplete():
     global wasMoving
@@ -62,52 +70,27 @@ def checkMotionComplete():
     wasMoving = isMoving
     return retVal
 
-#Define stage 1
+#TODO: complete handle
+# Define stage 1
 def handleLeft1():
     global currentState
     global rackLevel
     
-    if(currentState == IDLE):
-        print("IDLE -> GOTOSTAGE")
-        currentState = GOTOSTAGE
-        rackLevel = 1
-        goToStage(1)
-    else: 
-        print(' -> IDLE')
-        currentState = IDLE
-        leftMotor.stop()
-        rightMotor.stop()
+    pass
 
-#Define stage 2
+#TODO: complete handle
+# Define stage 2
 def handleLeft2():
     global currentState
     global rackLevel
     
-    if(currentState == IDLE):
-        print("IDLE -> GOTOSTAGE")
-        currentState = PICKFRUIT
-        rackLevel = 2
-        goToStage(2)
-    else: 
-        print(' -> IDLE')
-        currentState = IDLE
-        leftMotor.stop()
-        rightMotor.stop()
+    pass
 
+#TODO: Goes to next state after motion is complete
 def handleMotionComplete():
     global currentState
+    pass
 
-    if(currentState == GOTOSTAGE):
-        print(' -> DRIVETOFRUIT')
-        currentState = DRIVETOFRUIT
-        driveToFruit(REVERSE)
-    elif(currentState == DRIVETOFRUIT):
-        print(' -> PICKFRUIT')
-        currentState = PICKFRUIT
-        pickFruit()
-    elif(currentState == PICKFRUIT):
-        print(' -> IDLE')
-        currentState = IDLE
         
 def goToStage(stageNum):
     global currentState
@@ -116,22 +99,24 @@ def goToStage(stageNum):
         rackMove(FORWARD, speedOfRack, levelOne)
     elif(stageNum == 2):
         rackMove(FORWARD, speedOfRack, levelTwo)
-        
+    
+#Returns rack to initial resting position  
 def returnRackToBase(stageNum):
-    global currentState
-    #Need to adjust distance values for different stages
-    if(stageNum == 1):
-        rackMove(REVERSE, speedOfRack, levelOne)
-    elif(stageNum == 2):
-        rackMove(REVERSE, speedOfRack, levelTwo)
+    pass
 
+#Drive toward fruit from set position
 def driveToFruit(direction):
-    drive(direction, speedOfTravel, driveDistance) #Adjust distance as needed
+    drive(direction, speedOfTravel, toFruitDistance) #Adjust distance as needed
 
+#Returns rack to base to pick fruit
 def pickFruit():
     returnRackToBase(rackLevel)
-    #drive(FORWARD, speedOfTravel, driveDistance) #Adjust distance as needed
-    
+  
+#TODO: Needs completed
+#Go back to rack position after depositing fruit  
+def returnRobotToBase():
+    pass
+
 controller.buttonL1.pressed(handleLeft1)
 controller.buttonL2.pressed(handleLeft2)
 
